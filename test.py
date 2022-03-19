3003bf0259264bf01f0302964bcd91aa1f3a389b
Kineticself <100250196+Kineticself@users.noreply.github.com>
2022-03-18 18:56:21 -0700

    dragged and dropped .PY file
    developer direction course for Python file.

---
+++ test.py
@@ -0,0 +1,25 @@
+print("Welcome to my python program! WOW")
+
+#print will tell the computer what to write 
+# variavbles xyz can be any name 
+# values are what the variable equals 
+
+x = "27"
+y = "Jacob is "
+z = "very much so cool! it is true!"
+
+#print your age
+def PrintAge():
+    print("this variable is: " + x)
+
+#print your name
+def PrintName():
+    print("this variable is:" + y)
+
+#print true/false if you are cool or not
+def PrintCoolStatus():
+    print("this variable is: " + z)
+
+PrintAge()
+PrintName()
+PrintCoolStatus()
